0b28b6d615b46eb1277536aa82b63d39c4e2b34f
Sirito <shehabelkhaligy100@gmail.com>
2022-11-09 02:58:39 +0200

    TIM Interface and program.c

--- STM32_drivers/src/TIMER_program.c
+++ STM32_drivers/src/TIMER_program.c
@@ -7,3 +7,10 @@
 #include "TIMER_config.h"
 #include "TIMER_private.h"
 
+void TIM_Init(void){
+    TIM2_PER->CR1_REG |= 0b00 << 5; // Edge Aligned Mode
+    TIM2_PER->CR1_REG |= 0b0 << 4; // Direction upcounting
+    TIM2_PER->CR1_REG |= 0b1 << 0; // Enable timer
+
+    TIM2_PER->CCMR1_REG |= 0b110 << 4; // PWM mode 1 enable upcounting
+}
