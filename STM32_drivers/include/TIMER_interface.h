21adb55257556f9c644a22e8aaafdf99e8e4acfb
Sirito <shehabelkhaligy100@gmail.com>
2022-11-09 04:49:06 +0200

    Fixed TIMER_interface.h and TIMER_private.h

--- STM32_drivers/include/TIMER_interface.h
+++ STM32_drivers/include/TIMER_interface.h
@@ -1,16 +1,14 @@
-#ifndef TIMER_INTERFACE_H_
-#define TIMER_INTERFACE_H_
+#ifndef TIMER_INTERFACE
+#define TIMER_INTERFACE
 
 /*
-CR1  DIR Bit When Low then upcounting
-ARR  Sets the freq
-CCR  Sets the duty cycle 
-OCx  for polarity
-CCMR by writing 110 in OCxM bits for Mode 1 
-CCMR Setting OCxPE bit and eventually the auto-reload preload register by setting the ARPE bit in the TIMx_CR1 register
-CCER CCxP bit for OCx polarity is software programmable
-In PWM mode (1 or 2), 
-TIMx_CNT and TIMx_CCRx are always compared to determine whether TIMx_CCRx≤TIMx_CNT or TIMx_CNT≤TIMx_CCRx
+CR1  DIR Bit When Low then upcounting (done)
+ARR  Sets the freq ()
+CCR  Sets the duty cycle () 
+CCMR by writing 110 in OCxM bits for Mode 1 (done) 
+CCMR Setting OCxPE bit and eventually the auto-reload preload register by setting the ARPE bit in the TIMx_CR1 register(done)
+CCER CCxP bit for OCx polarity is software programmable (done)
+In PWM mode (1 or 2),TIMx_CNT and TIMx_CCRx are always compared to determine whether TIMx_CCRx≤TIMx_CNT or TIMx_CNT≤TIMx_CCRx
 (depending on the direction of the counter).
 */
 
@@ -20,4 +18,4 @@ TIMx_CNT and TIMx_CCRx are always compared to determine whether TIMx_CCRx≤TIMx
  */
 void TIM_Init(void);
 void TIM_PWMSet(u32 PWM);
-#endif /* TIMER_INTERFACE_H_ */
+#endif /* TIMER_INTERFACE */
--- STM32_drivers/include/TIMER_private.h
+++ STM32_drivers/include/TIMER_private.h
@@ -14,10 +14,14 @@ typedef struct{
     volatile u32 CNT_REG;
     volatile u32 PSC_REG;
     volatile u32 ARR_REG;
+    volatile u32 Reserved1;
     volatile u32 CCR1_REG;
     volatile u32 CCR2_REG;
     volatile u32 CCR3_REG;
     volatile u32 CCR4_REG;
+    volatile u32 Reserved2;
+    volatile u32 DCR_REG;
+    volatile u32 DMAR_REG;
 }TIMER_Port_t;
 
 #define TIM2_PER ((volatile TIMER_Port_t *)0x40000000)
