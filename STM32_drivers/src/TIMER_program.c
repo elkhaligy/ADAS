f309fa173459c18fb12b8a5697e179db7bab6035
Sirito <shehabelkhaligy100@gmail.com>
2022-11-09 04:27:30 +0200

    Final TIMER_program.c

--- STM32_drivers/src/TIMER_program.c
+++ STM32_drivers/src/TIMER_program.c
@@ -10,7 +10,18 @@
 void TIM_Init(void){
     TIM2_PER->CR1_REG |= 0b00 << 5; // Edge Aligned Mode
     TIM2_PER->CR1_REG |= 0b0 << 4; // Direction upcounting
-    TIM2_PER->CR1_REG |= 0b1 << 0; // Enable timer
+    TIM2_PER->CR1_REG |= 0b1 << 7; // Auto reload enable
 
     TIM2_PER->CCMR1_REG |= 0b110 << 4; // PWM mode 1 enable upcounting
+    TIM2_PER->CCMR1_REG |= 0b1 << 3; // preload registr on ccr1 enable
+
+    TIM2_PER->CCER_REG |= 0b1 << 0; // capture compare output 1 enabled
+
+    TIM2_PER->CNT_REG = 0xFFFF; // Load cnt with maxmum value
+    TIM2_PER->DMAR_REG = 0b1;
+    TIM2_PER->CR1_REG |= 0b1 << 0; // Enable timer
+}
+
+void TIM_PWMSet(u32 PWM){
+    TIM2_PER->CCR1_REG |= 0x00FF;
 }
