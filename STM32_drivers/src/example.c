e79ecf1f6730839ac7da9f91a322af36b2f8c530
sirito97 <66971290+sirito97@users.noreply.github.com>
2022-12-05 03:25:40 +0200

    Fixed timer 3, now ultrasonic runs on timer3 (#25)

--- STM32_drivers/src/example.c
+++ STM32_drivers/src/example.c
@@ -11,16 +11,16 @@
 
 #include "H_BRIDGE/H_BRIDGE_interface.h"
 #include "ULTRASONIC/UltraSonic_interface.h"
-u32 Ticks = 0;
-u8 seconds = 0;
-float Time;
-float Distance;
+
+u16 Ticks = 0;
+u32 Distance;
+
 int main(void)
 {
     RCC_Init(); // system internal clock
     RCC_PeripheralClockEnable(RCC_APB2, RCC_GPIOA);
     RCC_PeripheralClockEnable(RCC_APB2, RCC_GPIOC);
-    RCC_PeripheralClockEnable(RCC_APB1, RCC_TIM3); //RCC for Timer 3
+    RCC_PeripheralClockEnable(RCC_APB1, RCC_TIM3); // RCC for Timer 3
     USART_Start(BAUD_RATE_115200, USART_1);
     GPIO_SetPinMode(GPIO_PORTC, PIN13, GPIO_OUTPUT_GP_PP_10MHZ); // built-in LED
     GPIO_SetPinValue(GPIO_PORTC, PIN13, GPIO_HIGH);
@@ -32,10 +32,14 @@ int main(void)
     EXTI_void_SelectLineTriggerType(0, EXTI_LineTrigger_RISING); // select trigger type (rising edge)
     NVIC_voidEnableInterrupt(6);                                 // enable interrupt 6 (EXTI0)
 
+    TIMER_Init(TIMER_NUM_3, 1);
     UltrSonic_INIT();
-
+    GPIO_SetPinValue(GPIO_PORTC, PIN13, 0);
+    
     while (1)
     {
+
+
         UltraSonic_Trigger();
         if (Distance < 5)
         {
@@ -45,7 +49,19 @@ int main(void)
         {
             GPIO_SetPinValue(GPIO_PORTC, PIN13, 1);
         }
-        // USART_SendByte(USART_1,Time);
+        
+        USART_SendByte(USART_1, Distance);
+        TICK_Delay(100);
+        
+        // // USART_SendByte(USART_1, Ticks);
+        // // TICK_Delay(100);
+        // TIMER_startCounting(TIMER_NUM_3);
+       
+        // while(TIMER_elapsedTicks(TIMER_NUM_3)<60000){
+        //     //USART_SendByte(USART_1, 0);
+        // }
+        // TIMER_stopCounting(TIMER_NUM_3);
+        // GPIO_SetPinValue(GPIO_PORTC, PIN13, 1);
     }
     return 0;
 }
@@ -55,21 +71,20 @@ void EXTI0_IRQHandler(void)
     static u8 f = 0;
     if (f == 0)
     {
+        //TICK_StartCounting();
         TIMER_startCounting(TIMER_NUM_3);
         EXTI_void_SelectLineTriggerType(0, EXTI_LineTrigger_FALLING);
-
         f = 1;
     }
     else if (f == 1)
     {
-
+        //Ticks = TICK_ElapsedTicks();
         Ticks = TIMER_elapsedTicks(TIMER_NUM_3);
-        Time = (float)Ticks / 1000000;
-        Distance = (Time / 2) * 34300;
+        Distance = ((Ticks / 2) * 34300) / 8000000;
+        //TICK_Stop();
         TIMER_stopCounting(TIMER_NUM_3);
         f = 0;
         EXTI_void_SelectLineTriggerType(0, EXTI_LineTrigger_RISING);
-        // GPIO_SetPinValue(GPIO_PORTC, PIN13, x);
     }
 
     EXTI_void_ClearFlag(0);
