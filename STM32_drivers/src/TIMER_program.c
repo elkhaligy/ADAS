d982eef3e70792beebaf1e7615c18c0038c1964d
Sirito <shehabelkhaligy100@gmail.com>
2022-12-05 03:24:51 +0200

    Fixed timer 3, now ultrasonic runs on timer3

--- STM32_drivers/src/TIMER_program.c
+++ STM32_drivers/src/TIMER_program.c
@@ -9,8 +9,8 @@
 
 void TIMER_Init(u8 TIMER_NUMBER, u8 TIMER_CHANNEL)
 {
- switch (TIMER_NUMBER)
- {
+    switch (TIMER_NUMBER)
+    {
     case TIMER_NUM_2:
         TIMER2_PER->CR1_REG |= 0b00 << 5; // Edge Aligned Mode
         TIMER2_PER->CR1_REG |= 0b0 << 4;  // Direction upcounting
@@ -18,37 +18,40 @@ void TIMER_Init(u8 TIMER_NUMBER, u8 TIMER_CHANNEL)
         switch (TIMER_CHANNEL)
         {
         case TIMER_CHANNEL_1:
-        TIMER2_PER->CCMR1_REG |= 0b110 << 4; // PWM mode 1 enable upcounting
-        TIMER2_PER->CCMR1_REG |= 0b1 << 3;   // Output compare 1 preload enable
-        TIMER2_PER->CCER_REG |= 0b1 << 0;    // signal is output on the corresponding output pin.
-        break;
+            TIMER2_PER->CCMR1_REG |= 0b110 << 4; // PWM mode 1 enable upcounting
+            TIMER2_PER->CCMR1_REG |= 0b1 << 3;   // Output compare 1 preload enable
+            TIMER2_PER->CCER_REG |= 0b1 << 0;    // signal is output on the corresponding output pin.
+            break;
         case TIMER_CHANNEL_2:
-        TIMER2_PER->CCMR1_REG |= 0b110 << 12;
-        TIMER2_PER->CCMR1_REG |= 0b1 << 11;
-        TIMER2_PER->CCER_REG |= 0b1 << 4;
-        break;
+            TIMER2_PER->CCMR1_REG |= 0b110 << 12;
+            TIMER2_PER->CCMR1_REG |= 0b1 << 11;
+            TIMER2_PER->CCER_REG |= 0b1 << 4;
+            break;
         case TIMER_CHANNEL_3:
-        TIMER2_PER->CCMR2_REG |= 0b110 << 4;
-        TIMER2_PER->CCMR2_REG |= 0b1 << 3;
-        TIMER2_PER->CCER_REG |= 0b1 << 8;
-        break;
+            TIMER2_PER->CCMR2_REG |= 0b110 << 4;
+            TIMER2_PER->CCMR2_REG |= 0b1 << 3;
+            TIMER2_PER->CCER_REG |= 0b1 << 8;
+            break;
         case TIMER_CHANNEL_4:
-        TIMER2_PER->CCMR2_REG |= 0b110 << 12;
-        TIMER2_PER->CCMR2_REG |= 0b1 << 11;
-        TIMER2_PER->CCER_REG |= 0b1 << 12;
-        break;
+            TIMER2_PER->CCMR2_REG |= 0b110 << 12;
+            TIMER2_PER->CCMR2_REG |= 0b1 << 11;
+            TIMER2_PER->CCER_REG |= 0b1 << 12;
+            break;
         }
-        TIMER2_PER->CNT_REG = 0xFFFF; // Load cnt with maximum value
-        TIMER2_PER->DMAR_REG = 0b1; // DMA Bit
+        TIMER2_PER->CNT_REG = 0xFFFF;    // Load cnt with maximum value
+        TIMER2_PER->DMAR_REG = 0b1;      // DMA Bit
         TIMER2_PER->CR1_REG |= 0b1 << 0; // Enable timer
-    break;
+        break;
     case TIMER_NUM_3:
-        TIMER3_PER->CR1_REG |= 0b00 << 5; // Edge Aligned Mode
-        TIMER3_PER->CR1_REG |= 0b1 << 4;  // Direction downcounting
-        TIMER3_PER->CR1_REG |= 0b1 << 7;  // Auto reload enable        
-    break;
- }
-    
+        CLR_BIT(TIMER3_PER->CR1_REG, 5); // Edge Aligned Mode
+        CLR_BIT(TIMER3_PER->CR1_REG, 6);
+        CLR_BIT(TIMER3_PER->CR1_REG, 4); // Direction downcounting
+
+        //SET_BIT(TIMER3_PER->CR1_REG, 7); // Auto reload enable
+
+        //TIMER3_PER->PSC_REG = 0x00000007; // Set prescaler to 8
+        break;
+    }
 }
 
 void TIMER_PWM(u8 PWM_PERCENTAGE, u8 TIMER_NUMBER, u8 TIMER_CHANNEL)
@@ -79,37 +82,40 @@ void TIMER_startCounting(u8 TIMER_NUMBER)
 {
     switch (TIMER_NUMBER)
     {
-        case TIMER_NUM_3:
-            TIMER3_PER->ARR_REG  = 0xFFFF;
-            TIMER3_PER->CR1_REG |= 0b1 << 0;
+    case TIMER_NUM_3:
+        TIMER3_PER->ARR_REG = 0x0000FFFF;
+        SET_BIT(TIMER3_PER->CR1_REG, 0); // Enable counter
         break;
     }
 }
 
 u16 TIMER_elapsedTicks(u8 TIMER_NUMBER)
 {
-    switch (TIMER_NUMBER)
-    {
-        case TIMER_NUM_3:;
-            u16 num1 = 0;
-            u16 num2 = 0;
-            u16 result = 0;
-            num1 = TIMER3_PER->ARR_REG;
-            num2 = TIMER3_PER->CNT_REG;
-            result = num1 - num2;
-            return result;
-        break;
-    }
-    return 0;
+    u16 ticks = 0;
+    ticks = TIMER3_PER->CNT_REG;
+    return ticks;
+    // switch (TIMER_NUMBER)
+    // {
+    // case TIMER_NUM_3:
+    //     return TIMER3_PER->CNT_REG;
+    //     // u16 num1 = 0;
+    //     // u16 num2 = 0;
+    //     // u16 result = 0;
+    //     // num1 = TIMER3_PER->ARR_REG;
+    //     // num2 = TIMER3_PER->CNT_REG;
+    //     // result = num1 - num2;
+    //     // return result;
+    //     // break;
+    // }
 }
 
 void TIMER_stopCounting(u8 TIMER_NUMBER)
-{   
+{
     switch (TIMER_NUMBER)
     {
-        case TIMER_NUM_3:
-            TIMER3_PER->CR1_REG |= 0b0 << 0;
-            TIMER3_PER->CNT_REG  = 0;
+    case TIMER_NUM_3:
+        CLR_BIT(TIMER3_PER->CR1_REG, 0); // Disable counter
+        TIMER3_PER->CNT_REG = 0x00000000;
         break;
     }
 }
